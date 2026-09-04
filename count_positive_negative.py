392f5b7889535d49c7c5b920db35359b7e54363f
nikhilkondapalli05-arch <nikhilkondapalli05@gmail.com>
2026-09-04 20:34:23 +0530

    Create count_positive_negative.py

---
+++ count_positive_negative.py
@@ -0,0 +1,12 @@
+numbers = [10, -5, 20, -8, 0, 15, -3]
+
+positive_count = 0
+negative_count = 0
+
+for number in numbers:
+  if number > 0:
+    positive_count+=1
+  if number < 0:
+    negative_count+=1
+print("Positive numbers:", positive_count)
+print("Negative numbers:", negative_count)  
